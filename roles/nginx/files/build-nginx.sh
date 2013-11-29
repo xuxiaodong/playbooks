c9aa2082db31f6f8e166ba1c3d28b011c706cc73
Xiaodong Xu <xxdlhy@gmail.com>
2013-11-29 10:53:29 +0800

    Update roles

--- roles/nginx/files/build-nginx.sh
+++ roles/nginx/files/build-nginx.sh
@@ -6,6 +6,7 @@ cd /tmp/nginx
     --with-http_stub_status_module \
     --with-http_ssl_module \
     --with-http_geoip_module \
-    --add-module=../lua-nginx-module
+    --add-module=../lua-nginx-module \
+    --add-module=../cache-purge
 
 make &&  make install
